cd42d816bc524239da17db400199284dfa479680
QUANG221222 <nguyennhatquang.2509@gmail.com>
2025-10-30 18:52:50 +0700

    feat: implement authentication and authorization features with JWT, including login, logout, and token refresh functionality

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -5,9 +5,15 @@ import {
   CreateUserRequest,
   CreateUserResponse,
   VerifyEmailRequest,
-  VerifyEmailResponse
+  VerifyEmailResponse,
+  LoginRequest,
+  LoginResponse,
+  RefreshTokenResponse,
+  RefreshTokenRequest
 } from '~/types/user.type'
 import { userService } from '~/services/user.service'
+import ms from 'ms'
+import { env } from '~/configs/environment'
 
 const createNew = async (
   req: Request<{}, {}, CreateUserRequest, {}>,
@@ -22,7 +28,7 @@ const createNew = async (
       data: createUser
     })
   } catch (error: any) {
-    next(new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, error.message))
+    next(error)
   }
 }
 
@@ -38,11 +44,92 @@ const verifyEmail = async (
       data: result
     })
   } catch (error: any) {
-    next(new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, error.message))
+    next(error)
+  }
+}
+
+const login = async (
+  req: Request<{}, {}, LoginRequest, {}>,
+  res: Response<LoginResponse>,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const result = await userService.login(req)
+
+    res.cookie('accessToken', result.accessToken, {
+      httpOnly: true, // security: only server can access cookie
+      secure: env.BUILD_MODE === 'production', // // true: only send cookie over HTTPS
+      sameSite: (env.BUILD_MODE === 'production' ? 'none' : 'lax') as
+        | 'none'
+        | 'lax', // CSRF protection
+      maxAge: ms('14 days')
+    })
+
+    res.cookie('refreshToken', result.refreshToken, {
+      httpOnly: true,
+      secure: env.BUILD_MODE === 'production',
+      sameSite: (env.BUILD_MODE === 'production' ? 'none' : 'lax') as
+        | 'none'
+        | 'lax',
+      maxAge: ms('14 days')
+    })
+
+    res.status(StatusCodes.OK).json({
+      message: 'Login successful',
+      data: result
+    })
+  } catch (error: any) {
+    next(error)
+  }
+}
+
+const logout = async (_req: Request, res: Response, next: NextFunction) => {
+  try {
+    // Clear cookies - accessToken & refreshToken
+    res.clearCookie('accessToken')
+    res.clearCookie('refreshToken')
+
+    res.status(StatusCodes.OK).json({ message: 'Logout successful' })
+  } catch (error: any) {
+    next(error)
+  }
+}
+
+const refreshToken = async (
+  req: Request<{}, {}, RefreshTokenRequest, {}>,
+  res: Response<RefreshTokenResponse>,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const result = await userService.refreshToken(req.cookies?.refreshToken)
+
+    res.cookie('accessToken', result.accessToken, {
+      httpOnly: true,
+      secure: env.BUILD_MODE === 'production',
+      sameSite: (env.BUILD_MODE === 'production' ? 'none' : 'lax') as
+        | 'none'
+        | 'lax',
+      maxAge: ms('14 days')
+    })
+
+    res.status(StatusCodes.OK).json({
+      message: 'Token refreshed successfully',
+      accessToken: result.accessToken
+    })
+  } catch (error: any) {
+    next(
+      new ApiError(
+        StatusCodes.FORBIDDEN,
+        'Please Sign In! (Error from refresh Token)'
+      )
+    )
   }
 }
 
 export const userController = {
   createNew,
-  verifyEmail
+  verifyEmail,
+  login,
+  logout,
+  refreshToken
 }
--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -36,3 +36,22 @@ export interface VerifyEmailResponse {
   message: string
   data: Partial<IUser>
 }
+
+export interface LoginRequest {
+  email: string
+  password: string
+}
+
+export interface LoginResponse {
+  message: string
+  data: Partial<IUser>
+}
+
+export interface RefreshTokenRequest {
+  refreshToken: string
+}
+
+export interface RefreshTokenResponse {
+  message: string
+  accessToken: string
+}
